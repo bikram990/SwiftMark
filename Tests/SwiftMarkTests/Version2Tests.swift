321fc605267a94b0efd11cb28fc3dd9ad13f286e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-02 16:56:22 -0500

    Created basic Version 2 test suite

---
+++ Tests/SwiftMarkTests/Version2Tests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import Version2
+
+class Version2Tests: XCTestCase {
+    let markdown = Markdown()
+    
+    func testText() {
+        let md = """
+        Hello World!
+        This is just text for know
+        """
+        
+        let html = """
+        Hello World!
+        This is just text for know
+        """
+        
+        XCTAssertEqual(try markdown.render(md), html)
+    }
+    
+    static var allTests : [(String, (Version2Tests) -> () throws -> Void)] {
+        return [
+            ("testText", testText)
+        ]
+    }
+}
