25cd5f9719cd8f1feae33a9e458a2afa7b56df3b
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-05 12:48:04 -0500

    Created Link renderer

---
+++ Sources/Version2/Renderers/Link.swift
@@ -0,0 +1,89 @@
+//The MIT License (MIT)
+//
+//Copyright (c) 2017 Caleb Kleveter
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+public class Link: Renderer {
+    public var regex: RegEx = "\\[(.+)\\]\\((.+)\\)"
+    public var templates: [String] = ["$1", "$2"]
+    public var type: String = "link"
+    public var disallowedTags: [String] = ["header", "quote", "hr"]
+    public var renderer: MarkdownRenderer = Markdown()
+    
+    public required init() {
+        renderer.disallowedTags = disallowedTags
+    }
+    
+    public func tokenize(_ strings: [String])throws -> Token {
+        let text = try renderer.tokenize(strings[0])
+        let url = try renderer.tokenize(strings[1])
+        let value = text + [TextToken(value: ":break:")] + url
+        return LinkToken(value: value)
+    }
+    
+    public func parse(_ token: Token)throws -> Node {
+        guard case let TokenValue.array(tokens) = token.value else {
+            throw LinkRenderingError.tokenParse
+        }
+        let nodes = try self.renderer.parse(tokens)
+        return LinkNode(value: nodes)
+    }
+    
+    public func render(_ node: Node)throws -> String {
+        guard case let NodeValue.array(nodes) = node.value else {
+            throw LinkRenderingError.nodeRender
+        }
+        let linkData = nodes.split(whereSeparator: { node in
+            guard case let NodeValue.string(split) = node.value else {
+                return false
+            }
+            return split == ":break:"
+        })
+        
+        let text = try self.renderer.render(Array(linkData[0]))
+        let url = try self.renderer.render(Array(linkData[1]))
+        return "<a href=\"\(url)\">\(text)</a>"
+    }
+    
+    
+}
+
+public class LinkToken: Token {
+    public var renderer: Renderer.Type = Link.self
+    public var value: TokenValue
+    
+    public init(value: [Token]) {
+        self.value = .array(value)
+    }
+}
+
+public class LinkNode: Node {
+    public var renderer: Renderer.Type = Link.self
+    public var value: NodeValue
+    
+    public init(value: [Node]) {
+        self.value = .array(value)
+    }
+}
+
+public enum LinkRenderingError: Error {
+    case tokenParse
+    case nodeRender
+}
